b05d26320a6b12480cc223ec4b91c5defd683d1b
Josep <ponceespejojoserenato@gmail.com>
2026-06-27 13:12:18 -0500

    Fix CORS + NGINX + frontend API URL

--- js/config.js
+++ js/config.js
@@ -1,15 +1,11 @@
 const CONFIG = {
+  API_URL:
+    location.hostname === "localhost" ||
+    location.hostname === "127.0.0.1"
+      ? "http://localhost:8080/api/v1"
+      : "http://18.117.216.163/api/v1",
 
-    API_URL:
-        location.hostname === "localhost" ||
-            location.hostname === "127.0.0.1"
-            ? "http://localhost:8080/api/v1"
-            : "http://18.117.216.163:8080/api/v1",
-
-    getData() {
-        return JSON.parse(
-            localStorage.getItem("data")
-        );
-    }
-
+  getData() {
+    return JSON.parse(localStorage.getItem("data"));
+  }
 };
