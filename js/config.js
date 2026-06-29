2d4d37dec5f6a5a27091368c2d1912206de530fa
Josep <ponceespejojoserenato@gmail.com>
2026-06-28 21:53:16 -0500

    fix api url remove 8080 nginx

--- js/config.js
+++ js/config.js
@@ -3,7 +3,7 @@ const CONFIG = {
     location.hostname === "localhost" ||
     location.hostname === "127.0.0.1"
       ? "http://localhost:8080/api/v1"
-      : "http://18.117.216.163/api/v1",
+      : "https://erpinventario.duckdns.org/api/v1",
 
   getData() {
     return JSON.parse(localStorage.getItem("data"));
